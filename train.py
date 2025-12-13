8a50ff911b22ec2df133c500348ab2d907ecee82
Alexandre Lindsey <joueur.galactic@gmail.com>
2025-12-13 18:32:26 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,21 @@
-import pickle
+import numpy as np
 
+import pickle
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
 
 from architecture import PacmanNetwork
 from data import PacmanDataset
+
+ACTION_INDEX = {
+    'North': 0,
+    'South': 1,
+    'East': 2,
+    'West': 3,
+    'Stop': 4
+}
 
 
 class Pipeline(nn.Module):
@@ -19,15 +30,51 @@ class Pipeline(nn.Module):
 
         self.path = path
         self.dataset = PacmanDataset(self.path)
-        self.model = PacmanNetwork()
+        self.model = PacmanNetwork(self.dataset[0][0].shape[0])
 
-        self.criterion = # ...
-        self.optimizer = # ...
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+
+    def actions_convert(actions):
+        actionsInd = []
+
+        for action in actions:
+            actionsInd.append(ACTION_INDEX[action])
+
+        return torch.tensor(actionsInd, dtype=torch.long)
+    actions_convert = staticmethod(actions_convert)
 
     def train(self):
         print("Beginning of the training of your network...")
 
-        # Your code here
+        self.model.train()
+        trainLoader = DataLoader(
+            self.dataset,
+            batch_size=32,
+            shuffle=True
+        )
+
+        epochsNum = 500
+
+        for epoch in range(epochsNum):
+            lossTotal = 0.0
+            batchesNum = 0
+
+            for batchInputs, batchActions in trainLoader:
+                batchActionsInd = Pipeline.actions_convert(batchActions)
+
+                self.optimizer.zero_grad()
+                batchOutputs = self.model(batchInputs)
+                batchLoss = self.criterion(batchOutputs, batchActionsInd)
+                batchLoss.backward()
+                self.optimizer.step()
+
+                lossTotal += batchLoss.item()
+                batchesNum += 1
+
+            lossAvg = lossTotal / batchesNum
+            if (epoch + 1) % 10 == 0:
+                print(f"Epoch [{epoch + 1}/{epochsNum}, Average Loss: {lossAvg:.4f}]")
 
         torch.save(self.model.state_dict(), "models/pacman_model.pth")
         print("Model saved !")
