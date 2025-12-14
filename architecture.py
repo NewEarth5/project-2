29fca18231ed6cf3c66c1fbc44f58547a1037eff
Alexandre Lindsey <joueur.galactic@gmail.com>
2025-12-14 02:57:43 +0100

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+from collections import OrderedDict
 import torch
 import torch.nn as nn
+
+from data import TENSOR_SIZE
 
 
 class PacmanNetwork(nn.Module):
@@ -9,44 +12,33 @@ class PacmanNetwork(nn.Module):
     Your neural network architecture.
     """
 
-    def __init__(self):
+    def __init__(self, layersNum=3):
         super().__init__()
-        input_size = 24
-        layer1_size = 128
-        layer2_size = layer1_size // 2
-        layer3_size = layer2_size // 2
-        output_size = 5
+        inputSize = TENSOR_SIZE
+        outputSize = 5
+        ordDict = OrderedDict()
+        layerSizePrev = inputSize
+        for i in range(layersNum):
+            if i == 0:
+                layerSize = inputSize * 3
+            else:
+                layerSize = layerSizePrev * 2 // 3
 
-        self.layer1 = nn.Linear(input_size, layer1_size)
-        self.normal1 = nn.BatchNorm1d(layer1_size)
-        self.dropout1 = nn.Dropout(0.3)
+            ordDict[f"layer{i + 1}"] = nn.Linear(layerSizePrev, layerSize)
+            ordDict[f"normal{i + 1}"] = nn.BatchNorm1d(layerSize)
+            ordDict[f"action{i + 1}"] = nn.GELU()
+            ordDict[f"dropout{i + 1}"] = nn.Dropout(0.3)
 
-        self.layer2 = nn.Linear(layer1_size, layer2_size)
-        self.normal2 = nn.BatchNorm1d(layer2_size)
-        self.dropout2 = nn.Dropout(0.3)
+            layerSizePrev = layerSize
 
-        self.layer3 = nn.Linear(layer2_size, layer3_size)
-        self.normal3 = nn.BatchNorm1d(layer3_size)
-        self.dropout3 = nn.Dropout(0.2)
+        ordDict["output"] = nn.Linear(layerSizePrev, outputSize)
 
-        self.output = nn.Linear(layer3_size, output_size)
-
-        # self.activation = nn.ReLU()
-        # self.activation = nn.LeakyReLU()
-        self.activation = nn.GELU()
-        # self.activation = nn.ELU()
+        self.flatten = nn.Flatten()
+        self.stack = nn.Sequential(ordDict)
 
     def forward(self, x):
-        x = self.activation(self.normal1(self.layer1(x)))
-        x = self.dropout1(x)
-
-        x = self.activation(self.normal2(self.layer2(x)))
-        x = self.dropout2(x)
-
-        x = self.activation(self.normal3(self.layer3(x)))
-        x = self.dropout3(x)
-
-        return self.output(x)
+        x = self.flatten(x)
+        return self.stack(x)
 
 
 if __name__ == "__main__":
